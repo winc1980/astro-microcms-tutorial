09fdac8a95baea5a50d73208b67583799d33fd06
Yuma Morimoto <kanten520212@gmail.com>
2024-03-20 19:01:32 +0900

    update

--- src/library/microcms.ts
+++ src/library/microcms.ts
@@ -2,7 +2,20 @@ import type { MicroCMSQueries } from "microcms-js-sdk";
 import { createClient } from "microcms-js-sdk";
 
 const client = createClient({
-  serviceDomain: process.env.MICROCMS_SERVICE_DOMAIN ?? '',
-  apiKey: process.env.MICROCMS_API_KEY ?? '',
+  serviceDomain: '7tbg8j90ay',
+  apiKey: '5uN4pRKxMayJcKhIylDkcTJgP9ivOQ22TmCa',
 });
+
+interface home {
+  title: string;
+  description :string;
+}
+
+export const fetchHomeData = async (): Promise<home> => {
+  const res = await client.get({
+    endpoint: 'home',
+    contentId: 'wo100da5ue'
+  })
+  return res;
+}
 
